909b0674ec68d879c58b0094322bed755e95b1dc
juan <jdaa9507@gmail.com>
2020-08-22 15:49:48 -0500

    Making the initial database and creatin the country class and countries controller. Countries CRUD

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        //Class Properties
+
+        // Primary key - an identity field
+        public int Id { get; set; }
+
+        // Country name field (required)
+        [MaxLength(50, ErrorMessage = "The field {0} must contain less than {1} characters" )]
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnSale.Common.Entities;
+
+namespace OnSale.Web.Data
+{
+    // herence 
+    public class DataContext : DbContext
+    {
+        // Constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        // Properties
+
+        // Generic property
+        public DbSet<Country> Countries { get; set; }
+
+        // method to prevent repeted countries
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // has an Index by field Name and that field is unique
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+
+    }
+}
