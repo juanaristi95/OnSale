3287d9e6d45802338144f316c651e8ab155d8124
juan <jdaa9507@gmail.com>
2020-08-23 18:15:13 -0500

    Add severals Images to product

--- OnSale.Web/Helpers/IConverterHelper.cs
+++ OnSale.Web/Helpers/IConverterHelper.cs
@@ -1,6 +1,7 @@
 ﻿using OnSale.Common.Entities;
 using OnSale.Web.Models;
 using System;
+using System.Threading.Tasks;
 
 namespace OnSale.Web.Helpers
 {
@@ -9,6 +10,10 @@ namespace OnSale.Web.Helpers
         Category ToCategory(CategoryViewModel model, Guid imageId, bool isNew);
 
         CategoryViewModel ToCategoryViewModel(Category category);
+        Task<Product> ToProductAsync(ProductViewModel model, bool isNew);
+
+        ProductViewModel ToProductViewModel(Product product);
+
     }
 
 }
--- OnSale.Web/Startup.cs
+++ OnSale.Web/Startup.cs
@@ -38,6 +38,7 @@ namespace OnSale.Web
             services.AddTransient<SeedDb>();
             services.AddScoped<IBlobHelper, BlobHelper>();
             services.AddScoped<IConverterHelper, ConverterHelper>();
+            services.AddScoped<ICombosHelper, CombosHelper>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
