df4cfb489bd620010d8b2592c203c8093a2e2320
juan <jdaa9507@gmail.com>
2020-08-29 17:58:52 -0500

    Eddit User

--- OnSale.Web/Helpers/IUserHelper.cs
+++ OnSale.Web/Helpers/IUserHelper.cs
@@ -25,6 +25,12 @@ namespace OnSale.Web.Helpers
         Task<SignInResult> ValidatePasswordAsync(User user, string password);
 
         Task<User> AddUserAsync(AddUserViewModel model, Guid imageId, UserType userType);
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<User> GetUserAsync(Guid userId);
+
 
     }
 
